7793af1fbc7fd3fb5a9e080a8b66070f5657d52a
Jesus Sanchez <jesusgabriel2525@gmail.com>
2026-03-14 11:47:37 -0400

    feat: adding record and removing return to save method

--- src/main/java/com/jsanchez/inventory/infraestructure/adapter/out/event/SpringEventPublisherAdapter.java
+++ src/main/java/com/jsanchez/inventory/infraestructure/adapter/out/event/SpringEventPublisherAdapter.java
@@ -2,17 +2,15 @@ package com.jsanchez.inventory.infraestructure.adapter.out.event;
 
 import com.jsanchez.inventory.application.port.out.EventPublisherPort;
 import com.jsanchez.inventory.domain.event.ProductCreatedEvent;
+import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class SpringEventPublisherAdapter implements EventPublisherPort {
 
     private final ApplicationEventPublisher publisher;
-
-    public SpringEventPublisherAdapter(ApplicationEventPublisher publisher) {
-        this.publisher = publisher;
-    }
 
     @Override
     public void publish(ProductCreatedEvent event) {
